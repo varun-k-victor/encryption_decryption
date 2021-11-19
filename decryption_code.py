73fe273601b929683729a6722df6db8bf86ce4d3
varun-k-victor <varun7victor@gmail.com>
2021-11-19 14:24:36 +0530

    minor improvement

--- decryption_code.py
+++ decryption_code.py
@@ -6,44 +6,52 @@ import pickle
 def main():
 	print("loading ciphertext_2.dat...")
 	time.sleep(3)
-	file2=open("ciphertext_2.dat","rb")
-	encrypted_2=pickle.load(file2)
-	key_2=pickle.load(file2)
-	iv=pickle.load(file2)
-	file2.close()
-	print("\n")
-
-	print("AES decryption")
-	time.sleep(2)
-	print("decrypting text...")
-	time.sleep(3)
-	aes=AES.new(key_2,AES.MODE_CBC,iv)
-	decrypted_2=aes.decrypt(encrypted_2)
-	unpadded_text=unpad(decrypted_2,16)
-	decoded_text=unpadded_text.decode()
-	print("AES decryption successful")
-	print("decrypted text: ",decoded_text)
-	print("\n")
-
-	print("loading ciphertext_1.txt...")
-	time.sleep(3)
-	file1=open("ciphertext_1.txt","r")
-	key_1=int(file1.read())
-	file1.close()
-	print("\n")
-
-	print("caesor decryption")
-	time.sleep(2)
-	print("decrypting text....")
-	time.sleep(3)
-	if(key_1>9 or key_1<1):
-		print("caesor key should be a number between 1 and 9")
+	try:
+		file2=open("ciphertext_2.dat","rb")
+	except FileNotFoundError:
+		print("ciphertext_2.dat not found")
 	else:
-		decrypted_1=""
-		for i in decoded_text:
-			decrypted_1=decrypted_1+chr(ord(i)-key_1)
-	print("caesor decryption successful")
-	print("decrypted text: ",decrypted_1)
+		encrypted_2=pickle.load(file2)
+		key_2=pickle.load(file2)
+		iv=pickle.load(file2)
+		file2.close()
+		print("\n")
+
+		print("AES decryption")
+		time.sleep(2)
+		print("decrypting text...")
+		time.sleep(3)
+		aes=AES.new(key_2,AES.MODE_CBC,iv)
+		decrypted_2=aes.decrypt(encrypted_2)
+		unpadded_text=unpad(decrypted_2,16)
+		decoded_text=unpadded_text.decode()
+		print("AES decryption successful")
+		print("decrypted text: ",decoded_text)
+		print("\n")
+
+		print("loading ciphertext_1.txt...")
+		time.sleep(3)
+		try:
+			file1=open("ciphertext_1.txt","r")
+		except FileNotFoundError:
+			print("ciphertext_1.txt not found")
+		else:
+			key_1=int(file1.read())
+			file1.close()
+			print("\n")
+
+			print("caesor decryption")
+			time.sleep(2)
+			print("decrypting text....")
+			time.sleep(3)
+			if(key_1>9 or key_1<1):
+				print("caesor key should be a number between 1 and 9")
+			else:
+				decrypted_1=""
+				for i in decoded_text:
+					decrypted_1=decrypted_1+chr(ord(i)-key_1)
+			print("caesor decryption successful")
+			print("decrypted text: ",decrypted_1)
 	
 if __name__=="__main__":
 	main()
